613ae043c3e8ba0e27473c098eb02f3c833d22a1
Erwin Fischer <erfisch@gmail.com>
2020-07-09 18:21:19 -0400

    se agrega funcion

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
 from flask import Flask
 app = Flask(__name__)
 
+@app.route('/')
+def principal():
+	return "hola"
+	
 @app.route('/suma/<int:s1>/<int:s2>',methods=['GET'])
 def suma(s1,s2):
     return str(s1+s2)
+	
+@app.route('/resta/<int:s1>/<int:s2>',methods=['GET'])
+def resta(s1,s2):
+    return str(s1-s2)
 
 if __name__ == '__main__':
     app.run()
