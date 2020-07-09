8da284459877d5973975b62d850e6d4ef08c4dec
Juan Cares <42586131+WoozieJuank@users.noreply.github.com>
2020-07-09 15:08:42 -0400

    restar
    a{adir restar

--- app.py
+++ app.py
@@ -9,5 +9,9 @@ def suma(s1,s2):
 def dividir(s1,s2):
     return str(s1/s2)
 
+@app.route('/restar/<int:s1>/<int:s2>',methods=['GET'])
+def restar(s1,s2):
+    return str(s1-s2)
+
 if __name__ == '__main__':
     app.run()
--- calculadora.py
+++ calculadora.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def suma(s1,s2):
     return str(s1+s2)
 
+@app.route('/restar/<int:s1>/<int:s2>',methods=['GET'])
+def restar(s1,s2):
+    return str(s1-s2)
+
 if __name__ == '__main__':
     app.run()
